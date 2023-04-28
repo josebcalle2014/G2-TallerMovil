d17eeee32962ba40608d3b81f78f2eda03e3c568
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-04-28 09:02:05 -0500

    Add compose dependencies

--- app/src/main/java/com/example/appclinicaunmsm/presentacion/MainActivity.kt
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.appclinicaunmsm.presentacion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.appclinicaunmsm.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Greeting("Android")
+        }
     }
 }
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Android")
+}
+
