7037e5da5742efcdb04b5bf3c92ba4782f0df07f
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-05-03 21:49:11 -0500

    Agregando arreglo para botones de la barra inferior

--- app/src/main/java/com/example/appclinicaunmsm/presentacion/utils/BottomBar.kt
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/utils/BottomBar.kt
@@ -8,14 +8,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+private val navBarItems = arrayOf(
+    Pair("Inicio", Icons.Outlined.Home),
+    Pair("Citas", Icons.Outlined.AccountCircle),
+    Pair("Historial", Icons.Outlined.Info),
+    Pair("Configuración", Icons.Outlined.Settings),
+)
 
 @Composable
 @Preview
@@ -26,25 +40,27 @@ fun BottomBar() {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            SectionBottomBar("Boton 1")
-            SectionBottomBar("Boton 2")
-            SectionBottomBar("Boton 3")
-            SectionBottomBar("Boton 4")
+            for (item in navBarItems) {
+                SectionBottomBar(item.first, item.second)
+            }
         }
     })
 }
 
 @Composable
-fun SectionBottomBar(text: String) {
+fun SectionBottomBar(text: String, imageVector: ImageVector) {
     Column(
         modifier = Modifier.padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Button(
+        IconButton(
             modifier = Modifier.size(20.dp),
             onClick = { /*TODO*/ }
         ) {
-
+            Icon(
+                imageVector = imageVector,
+                contentDescription = "dummy",
+            )
         }
         Text(
             fontSize = 12.sp,
