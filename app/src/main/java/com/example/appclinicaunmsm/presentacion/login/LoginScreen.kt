52f3be305ea503a5288fde6fd42b10b0e642b319
Luiggi Pasache <luiggi.pasache@unmsm.edu.pe>
2023-05-05 07:53:47 -0500

    Cambiando nombres de pantallas

--- app/src/main/java/com/example/appclinicaunmsm/presentacion/login/LoginScreen.kt
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/login/LoginScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.appclinicaunmsm.presentacion.login.components.InputForm
 import com.example.appclinicaunmsm.presentacion.login.components.PasswordInput
 import com.example.appclinicaunmsm.presentacion.login.components.UserInput
 
--- app/src/main/java/com/example/appclinicaunmsm/presentacion/welcome/WecolmeScreen.kt
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/welcome/WecolmeScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
