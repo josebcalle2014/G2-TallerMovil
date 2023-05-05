6663b193c43e99cc2d94c0f54483c0db5bae735c
Luiggi Pasache <luiggi.pasache@unmsm.edu.pe>
2023-05-05 08:15:16 -0500

    Agregar componente para botón de formulario

---
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/login/components/ButtonForm.kt
@@ -0,0 +1,27 @@
+package com.example.appclinicaunmsm.presentacion.login.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ButtonForm(onClick: () -> Unit) {
+    Button(onClick = onClick,
+        modifier = Modifier.fillMaxWidth()) {
+        Icon(
+            Icons.Filled.AccountBox,
+            contentDescription = "Favorite",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+        Text("Ingresar")
+    }
+}
