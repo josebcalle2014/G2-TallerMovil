c874af98389a253571aea668d63fcba063bff65b
Luiggi Pasache <luiggi.pasache@unmsm.edu.pe>
2023-05-05 08:16:04 -0500

    Reemplazando botón en LoginForm por componente de boton

--- app/src/main/java/com/example/appclinicaunmsm/presentacion/login/LoginScreen.kt
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/login/LoginScreen.kt
@@ -3,19 +3,13 @@ package com.example.appclinicaunmsm.presentacion.login
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +19,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.appclinicaunmsm.presentacion.login.components.ButtonForm
+import com.example.appclinicaunmsm.presentacion.login.components.ForgotPassword
 import com.example.appclinicaunmsm.presentacion.login.components.PasswordInput
 import com.example.appclinicaunmsm.presentacion.login.components.UserInput
 
@@ -76,15 +72,9 @@ fun LoginForm() {
             )
         }
 
-        Button(onClick = { showDialog.value = true }) {
-            Icon(
-                Icons.Filled.AccountBox,
-                contentDescription = "Favorite",
-                modifier = Modifier.size(ButtonDefaults.IconSize)
-            )
-            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-            Text("Ingresar")
-        }
+        ButtonForm(onClick = { showDialog.value = true })
+
+        ForgotPassword()
     }
 }
 
