cf9a0b9fd2a6a8d59b5f7ad7051208a5b1164b97
Luiggi Pasache <luiggi.pasache@unmsm.edu.pe>
2023-05-20 01:31:09 -0500

    Mejorando redirección y agregando previews

--- app/src/main/java/com/example/appclinicaunmsm/presentacion/global/bar/TopBar.kt
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/global/bar/TopBar.kt
@@ -1,14 +1,28 @@
 package com.example.appclinicaunmsm.presentacion.global.bar
 
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import com.example.appclinicaunmsm.R
 
-@Preview
 @Composable
-fun TopBar() {
-    TopAppBar(title = { Text(text = stringResource(R.string.app_name))})
+fun TopBar(navController: NavController) {
+    TopAppBar(title = { Text(text = stringResource(R.string.app_name)) },
+        navigationIcon = if (navController.previousBackStackEntry != null) {
+            {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack, contentDescription = "ArrowBack"
+                    )
+                }
+            }
+        } else { null }
+    )
+
 }
