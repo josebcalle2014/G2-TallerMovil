03a54561f2405c3c0e061aa3714bea512cf12d18
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-07-12 08:48:53 -0500

    Eliminando botón para retroceder

--- app/src/main/java/com/example/appclinicaunmsm/presentacion/global/barras/BarraSuperior.kt
+++ app/src/main/java/com/example/appclinicaunmsm/presentacion/global/barras/BarraSuperior.kt
@@ -1,15 +1,12 @@
 package com.example.appclinicaunmsm.presentacion.global.barras
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,27 +22,22 @@ import com.example.appclinicaunmsm.presentacion.tema.AppClinicaUnmsmTheme
 @Composable
 fun BarraSuperior(navController: NavController) {
     TopAppBar(
-        navigationIcon = if (navController.previousBackStackEntry != null) {
-            {
-                IconButton(onClick = { navController.popBackStack() }) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack, contentDescription = "ArrowBack"
-                    )
-                }
-            }
-        } else { null },
         title = {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 Text(text = stringResource(R.string.app_name))
                 Spacer(modifier = Modifier.weight(1f))
                 Button(onClick = {
                     navController.navigate(Vista.Login.route)
                     UsuarioSingleton.cerrarSesion()
-                } ) {
+                }) {
                     Text(text = "Salir")
                 }
             }
-        })
+        }
+    )
 }
 
 @Composable
